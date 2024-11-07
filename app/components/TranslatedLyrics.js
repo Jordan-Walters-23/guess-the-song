fbc51474e3dec9032e7cad82e6311d04baadaca8
Jordan Walters <128825567+Jordan-Walters-23@users.noreply.github.com>
2024-11-07 12:30:11 +0000

    Translation api route tested with Thunderclient

--- app/components/TranslatedLyrics.js
+++ app/components/TranslatedLyrics.js
@@ -1,13 +1,21 @@
-import React from 'react'
-
 export default async function TranslatedLyrics() {
-    let data = await fetch('https://api.vercel.app/blog')
-    let posts = await data.json()
-    return (
-      <ul>
-        {posts.map((post) => (
-          <li key={post.id}>{post.title}</li>
-        ))}
-      </ul>
-    )
-  }
+  let headersList = {
+    Accept: "application/json",
+    Authorization: `DeepL-Auth-Key ${process.env.TRANSLATION_API_KEY}`,
+    "Content-Type": "application/json",
+  };
+
+  let bodyContent = JSON.stringify({
+    text: ["More than enough"],
+    target_lang: "ES",
+  });
+
+  let response = await fetch("https://api-free.deepl.com/v2/translate", {
+    method: "POST",
+    body: bodyContent,
+    headers: headersList,
+  });
+
+  let data = await response.text();
+  return data;
+}
